b7bdbea3e448b2938cb7d258ba835aaddc8dbb3b
Jeff Squires <jeff.squires@gmail.com>
2012-02-19 18:51:31 -0500

    general improvements.  updated Versionfile to prepare for new spree-0-70-stable branch (and 1.0 upgrade)

--- app/assets/javascripts/store/spree_reuse_credit_card.js
+++ app/assets/javascripts/store/spree_reuse_credit_card.js
@@ -1,1 +1,12 @@
-//= require store/spree_core
+function displayCreditCardDeleteStatus(notice) {
+  notice_div = $('.flash.notice');
+
+  if notice {
+    if (notice_div.length > 0) {
+      notice_div.html(notice);
+      notice_div.show();
+    } else {
+      $("#card_notice").html('<div class="flash notice">' + notice + '</div>');
+    }
+  }
+}
