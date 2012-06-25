0e6dab6a18a0c898ac9082dbd66916d7a00da790
Jeff Squires <jeff.squires@gmail.com>
2012-06-25 14:01:51 -0400

    fixed flaw in sql statement that erroneously picked up payments w/ no source

--- lib/card_reuse.rb
+++ lib/card_reuse.rb
@@ -2,8 +2,8 @@ module CardReuse
   def all_cards_for_user(user)
     return nil unless user
 
-    orders = Order.where(:user_id => user.id).complete.order(:created_at).joins(:payments).where('payments.source_type' => 'Creditcard').where('payments.state' => 'completed')
-    payments = orders.map(&:payments).flatten
+    payments = Payment.joins(:order).where('orders.completed_at IS NOT NULL').where('orders.user_id' => user.id).order('orders.created_at').where('payments.source_type' => 'Creditcard').where('payments.state' => 'completed')
+
 
     payments.map do |payment| 
       src = payment.source
