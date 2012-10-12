8fd01b96b321c40355e3e6acc5ad648dfda552ed
Jeff Squires <jeff.squires@gmail.com>
2012-10-12 16:48:00 -0400

    Vastly improves the UI/UX

--- app/assets/javascripts/store/spree_reuse_credit_card.js
+++ app/assets/javascripts/store/spree_reuse_credit_card.js
@@ -13,3 +13,33 @@ function displayCreditCardDeleteStatus(notice) {
     }
   }
 }
+
+
+$('#use_existing_card_no').live('click', function () {
+                                       // why am i having to hide the contents of the div as well???
+                                       $("#existing_cards").hide();
+                                       $("#existing_cards h4").hide();
+                                       $("#existing_cards table").hide();
+
+                                       $("[data-hook=card_number]").show();
+                                       $("[data-hook=card_expiration]").show();
+                                       $("[data-hook=cart_code]").show(); // unfortunately this is a typo in spree (cart v card)
+});
+$('#use_existing_card_yes').live('click', function () {
+                                       $("#existing_cards").show();
+                                       $("#existing_cards h4").show();
+                                       $("#existing_cards table").show();
+
+                                       $("[data-hook=card_number]").hide();
+                                       $("[data-hook=card_expiration]").hide();
+                                       $("[data-hook=cart_code]").hide();
+});
+
+$('input[type=radio][name=existing_card]').live('change',function () {
+                                                  if ($(this).is(':checked')) {
+                                                    $(".form-buttons input[type=submit]").attr('disabled',false);
+                                                    $(".form-buttons input[type=submit]").val(original_button_text);
+                                                  }
+                                                }
+                                               );
+
