efb6531cc867415e6d4eca6862aca65cf905e4fa
Jeff Squires <jeff.squires@gmail.com>
2013-02-22 12:03:14 -0500

    fixes selector when deciding to enable/disable continue button

--- app/assets/javascripts/store/spree_reuse_credit_card.js
+++ app/assets/javascripts/store/spree_reuse_credit_card.js
@@ -74,10 +74,7 @@ $(document).on('change', 'input[type=radio][name=existing_card]',function () {
 $(document).on('click','input[type="radio"][name="order[payments_attributes][][payment_method_id]"]', function() {
   ($('#payment-methods li')).hide();
   if (this.checked) {
-    // why doesn't this work????
-    // if ($.contains($('#payment_method_' + this.value),$('#card_notice'))) {
-    // if ($('#payment_method_' + this.value).find('#card_notice').length > 0) {
-    if ($('.existing-credit-card-list').length > 0) {
+    if ($('#payment_method_' + this.value).parent('ul').find('#card_notice').length > 0) {
       disableContinueButton();
     } else {
       restoreContinueButton();
