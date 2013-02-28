bf07adf8dc78cc15b98f07761cce17657120c890
M. Scott Ford <scott@mscottford.com>
2013-02-27 23:09:01 -0500

    Cleans up spec_helper
    These changes were loosely based on the spec_helper.rb file in
    spree_auth_devise.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-
 require 'rspec/rails'
-require 'rspec/autorun'
+require 'database_cleaner'
+require 'spree/core/url_helpers'
+require 'ffaker'
 
 require 'factory_girl'
 
@@ -13,33 +14,13 @@ Dir["#{File.dirname(__FILE__)}/factories/**"].each do |f|
   require fp
 end
 
-
-require 'capybara/rspec'
-require 'capybara/rails'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
 
-# wanted to pull in spree's support files..but not in the gem
-module AuthenticationHelpers
-  def sign_in_as!(user)
-    visit '/login'
-    fill_in 'Email', :with => user.email
-    fill_in 'Password', :with => 'secret'
-    click_button 'Login'
-  end
-
-end
-
-# Requires factories defined in spree_core
+require 'spree/core/testing_support/fixtures'
 require 'spree/core/testing_support/factories'
-require 'spree/core/testing_support/env'
-#require 'spree/url_helpers'
-
-# requiring above didn't work
-def spree
-  Spree::Core::Engine.routes.url_helpers
-end
+require 'spree/core/testing_support/controller_requests'
 
 RSpec.configure do |config|
   # ## Mock Framework
@@ -57,12 +38,11 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
-
-  config.include AuthenticationHelpers, :type => :request
+  config.include FactoryGirl::Syntax::Methods
+  config.include Spree::Core::UrlHelpers
+  config.include Spree::Core::TestingSupport::ControllerRequests, :type => :controller
+  config.include Devise::TestHelpers, :type => :controller
+  config.include Rack::Test::Methods, :type => :requests
 end
