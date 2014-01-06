1803122abdce98e9ce2452c588ca3a1990e4ad43
Jeff Squires <jeff.squires@gmail.com>
2014-01-06 14:14:48 -0500

    don't show expired cards

--- lib/card_reuse.rb
+++ lib/card_reuse.rb
@@ -15,9 +15,16 @@ module CardReuse
     end.compact.uniq
   end
 
+  def card_expired?(payment_source)
+    exp = DateTime.new(payment_source.year.to_i, payment_source.month.to_i).end_of_month
+    now = DateTime.now.end_of_month
+
+    exp < now
+  end
+
   def valid_for_reuse?(payment_source)
     # some payment gateways use only one of these?  stripe possibly?
-    !(payment_source.nil? || (payment_source.gateway_payment_profile_id.nil? && payment_source.gateway_customer_profile_id.nil?) || payment_source.deleted?)
+    !(payment_source.nil? || (payment_source.gateway_payment_profile_id.nil? && payment_source.gateway_customer_profile_id.nil?) || payment_source.deleted? || card_expired?(payment_source))
   end
 
   def credit_card_payments_for_user(user)
